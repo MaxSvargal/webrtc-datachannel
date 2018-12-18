782699665e690e4eee19d9cb91e358927a58d078
MaxSvargal <maxsvargal@gmail.com>
2018-12-18 19:28:23 +0700

    Make lib universal and  response on messages in promises

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -1,37 +1,56 @@
 import { EventEmitter } from 'events'
 
-const wrtc = require('wrtc') // nodejs
+interface JRPCMessage {
+  nonce: number
+  message: string
+}
 
 interface IOptions {
   connection: RTCConfiguration
+  wrtc?: {
+    RTCPeerConnection: typeof RTCPeerConnection
+    RTCIceCandidate: typeof RTCIceCandidate
+  }
 }
 
 export default class WebRTC extends EventEmitter {
   private rpc: RTCPeerConnection
-  private candidates: RTCIceCandidate[]
-  private dataChannel: RTCDataChannel | null
+  private RTCIceCandidate: typeof RTCIceCandidate
+  private candidates: RTCIceCandidate[] = []
+  private alisteners: { [nonce: number]: Function } = {}
+  private dataChannel: RTCDataChannel | null = null
+  public messageNonce = 0
 
-  constructor({ connection }: IOptions) {
+  constructor({ connection, wrtc }: IOptions) {
     super()
-    this.candidates = []
-    this.dataChannel = null
-    this.rpc = new wrtc.RTCPeerConnection(connection)
-    this.rpc.onicecandidate = ({ candidate }) => (console.log({ candidate }), candidate && this.candidates.push(candidate))
-    this.rpc.onnegotiationneeded = () => this.emit('negotiationneeded')
+
+    const RTC = wrtc ? wrtc.RTCPeerConnection : RTCPeerConnection
+    this.RTCIceCandidate = wrtc ? wrtc.RTCIceCandidate : RTCIceCandidate
+
+    this.rpc = new RTC(connection)
+
+    this.rpc.onicecandidate = ({ candidate }) => {
+      if (candidate) {
+        this.candidates.push(candidate)
+        this.emit('icecandidate', candidate)
+      }
+    }
+
     this.rpc.ondatachannel = event => {
       this.dataChannel = event.channel
-      this.dataChannel.onmessage = (msg) => this.emit('msg', msg)
       this.emit('datachannel', event)
-    }
-    this.rpc.onconnectionstatechange = event => console.log('connectionstatechange', event)
+    }  
+    
+    this.rpc.onconnectionstatechange = event =>
+      this.emit('connectionstatechange', event)
   }
 
-  getCandidates = async (i = 0): Promise<string | null> => {
-    if (i >= 10) return null
-    if (this.candidates.length >= 3)
+  getCandidates = async (i = 0): Promise<string> => {
+    if (i >= 20) return Promise.reject('No connection.')
+    if (this.candidates.length > 0)
       return Promise.resolve(JSON.stringify(this.candidates))
     else {
-      await new Promise(resolve => setTimeout(resolve, 300))
+      await new Promise(resolve => setTimeout(resolve, 100))
       return await this.getCandidates(++i)
     }
   }
@@ -44,8 +63,23 @@ export default class WebRTC extends EventEmitter {
 
   createChannel = (name = 'chat') => {
     const channel = this.rpc.createDataChannel(name)
-    channel.onopen = () => this.emit('channelopen')
-    channel.onmessage = ({ data }) => this.emit('message', data)
+    channel.onopen = () => this.emit('open')
+    channel.onclose = () => this.emit('close')
+    channel.onerror = error => this.emit('error', error)
+    channel.onmessage = ({ data }: { data: string }) => {
+      try {
+        const msg: JRPCMessage = JSON.parse(data)
+        const { nonce, message } = msg
+        this.emit('message', msg)
+
+        if (this.alisteners[nonce]) {
+          this.alisteners[nonce](message)
+          delete this.alisteners[nonce]
+        }
+      } catch (err) {
+        console.error('Message couldn\'t be parsed. It has not a valid format.')
+      }
+    }
     return channel
   }
 
@@ -63,11 +97,23 @@ export default class WebRTC extends EventEmitter {
 
   addCandidates = async (candidates: string) => {
     const values = JSON.parse(candidates) as RTCIceCandidateInit[]
-    const promises = values.map((candidate) => this.rpc.addIceCandidate(new wrtc.RTCIceCandidate(candidate)))
+    const promises = values.map(candidate =>
+      this.rpc.addIceCandidate(new this.RTCIceCandidate(candidate)))
+
     await Promise.all(promises)
     return this.candidates
   }
 
-  sendMessage = (message: object) =>
-    this.dataChannel!.send(JSON.stringify(message))
+  send = (message: any): Promise<MessageEvent> =>
+    new Promise((resolve, reject) => {
+      if (!this.dataChannel)
+        return reject(Error('dataChannel doesn\'t opened'))
+
+      this.dataChannel.send(JSON.stringify({
+        nonce: ++this.messageNonce,
+        message
+      } as JRPCMessage))
+
+      this.alisteners[this.messageNonce] = resolve
+    })
 }
